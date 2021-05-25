71d4c29b217e1702f04236c11cec45571a00065f
BigLazyET <xjl505302554@outlook.com>
2021-05-25 18:06:06 +0800

    no message

---
+++ FinanicalDoraPocket/src/wpf/DoraPocket.View/SampleView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DoraPocket.View
+{
+    public partial class SampleView : Page
+    {
+        public SampleView()
+        {
+            InitializeComponent();
+        }
+    }
+}
