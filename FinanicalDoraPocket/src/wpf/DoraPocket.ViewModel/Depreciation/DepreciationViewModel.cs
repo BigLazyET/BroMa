132ee62d1e2465ac41f45b4562e442c7bb267d9a
BigLazyET <xjl505302554@outlook.com>
2021-05-28 16:38:39 +0800

    no message

--- FinanicalDoraPocket/src/wpf/DoraPocket.ViewModel/Depreciation/DepreciationViewModel.cs
+++ FinanicalDoraPocket/src/wpf/DoraPocket.ViewModel/Depreciation/DepreciationViewModel.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DoraPocket.ViewModel.Depreciation
+﻿namespace DoraPocket.ViewModel.Depreciation
 {
     public class DepreciationViewModel : BaseViewModel
     {
+        private string filePath;
+        private string sheetName;
+        private double originalValue;
+        private double residualValueRate;
+
+        /// <summary>
+        /// 需要进行折旧计算的excel文件路径
+        /// </summary>
+        public string FilePath
+        {
+            get { return filePath; }
+            set { SetProperty(ref filePath, value); }
+        }
+
+        /// <summary>
+        /// 需要进行折旧计算的sheet名
+        /// 若为空，则默认全部sheet都进行计算；若非空，多个待计算的sheet用英文','分隔
+        /// </summary>
+        public string SheetName
+        {
+            get { return sheetName; }
+            set { SetProperty(ref sheetName, value); }
+        }
+
+
+
     }
 }
--- FinanicalDoraPocket/src/wpf/DoraPocket.WPF/Views/Depreciation/DepreciationView.xaml.cs
+++ FinanicalDoraPocket/src/wpf/DoraPocket.WPF/Views/Depreciation/DepreciationView.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DoraPocket.ViewModel.Depreciation;
+using Microsoft.Win32;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DoraPocket.WPF.Views.Depreciation
 {
@@ -21,9 +11,32 @@ namespace DoraPocket.WPF.Views.Depreciation
     /// </summary>
     public partial class DepreciationView : Page
     {
+        private readonly DepreciationViewModel depreciationViewModel;
+
         public DepreciationView()
+
         {
             InitializeComponent();
+
+            depreciationViewModel = DataContext as DepreciationViewModel;
+        }
+
+        private void PickFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // configure open file dialog box
+            var dialog = new OpenFileDialog();
+            //dialog.FileName = "折旧"; // 默认文件名
+            //dialog.DefaultExt = ".xls"; // 默认文件扩展名
+            dialog.Filter = "All files(.)|*.*"; // 过滤扩展名
+
+            // show open file dialog box
+            bool? result = dialog.ShowDialog();
+
+            // process open file dialog box result
+            if (result == true)
+            {
+                depreciationViewModel.FilePath = dialog.FileName;
+            }
         }
     }
 }
